f87286c03c0b43fc3cd6c557708c35ff745bc86f
Nittya Gopal Baidya <mrnittya@gmail.com>
2025-01-10 17:50:06 +0530

    index.js updated

--- backend/index.js
+++ backend/index.js
@@ -22,9 +22,9 @@ app.use(express.json());//Parse incoming requests:req.body
 app.use(cookieParser());//Parse incoming cookies
 app.use("/api/auth",authRoutes)
 if(process.env.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname,"/frontend/dist")));
+  app.use(express.static(path.join(__dirname,"")));
   app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+    res.sendFile(path.resolve(__dirname,"frontend/dist/index.html"));
 
   });
 }
