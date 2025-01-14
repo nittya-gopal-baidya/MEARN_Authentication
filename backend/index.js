d5bed06170e61e1603395a08dca6931a275c3e35
Nittya Gopal Baidya <mrnittya@gmail.com>
2025-01-15 02:04:33 +0530

    ready to deploy

--- backend/index.js
+++ backend/index.js
@@ -22,10 +22,12 @@ app.use(express.json());//Parse incoming requests:req.body
 app.use(cookieParser());//Parse incoming cookies
 app.use("/api/auth",authRoutes);
 
-app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
 });
+}
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server running on port ${PORT}`);
